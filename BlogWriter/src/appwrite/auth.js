b63902190143031cbc10a4d5493049175195a35d
M-BharathKumar <bharathkuamarmittapalli2005@gmail.com>
2025-09-20 22:24:13 +0530

    Auth_set_Up

---
+++ BlogWriter/src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import conf from "../config/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login(email, password);
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (err) {
+      console.log("App Write Service :: getUser ::error", err);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("App Write Service :Logout:error", error);
+    }
+  }
+}
+const authService = new AuthService();
+export default authService;
